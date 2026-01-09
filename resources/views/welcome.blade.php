15250ea49baa68e50f5955de97c7f6c70c5f7e13
dcorralf <davidcorralfiestas@gmail.com>
2026-01-09 21:14:42 +0100

    Adding some modifications in some files. Build the env.example file.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,9 @@
-{{-- Extiende el layout principal para tener el mismo estilo y barra de navegación --}}
 @extends('layouts.app')
 
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                {{-- Usamos un componente "jumbotron" (de Bootstrap) para el mensaje principal --}}
                 <div class="p-5 mb-4 bg-light rounded-3 text-center">
                     <div class="container-fluid py-5">
                         <h1 class="display-5 fw-bold">Bienvenido a Mi Aplicación</h1>
